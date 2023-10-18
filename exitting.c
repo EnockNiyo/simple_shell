b2f87c56118dc65d86bc319350f078c651cd0e04
ngabo-dev <ngabo470@gmail.com>
2023-10-18 03:28:56 -0700

    exiting

--- exitting.c
+++ exitting.c
@@ -55,3 +55,18 @@ char *_strncating(char *dest, char *src, int n)
 		dest[i] = '\0';
 	return (s);
 }
+/**
+ **_strchrs - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+ */
+char *_strchrs(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
