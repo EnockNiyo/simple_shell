96c5d0b4ec421d55642f7123f159853d61097161
ngabo-dev <ngabo470@gmail.com>
2023-10-18 03:10:44 -0700

    finding errors

--- error.c
+++ error.c
@@ -1,86 +1,20 @@
-nclude "shell.h"
+#include "shell.h"
 
 /**
- *  * _eputses - prints an input string
- *   * @str: the string to be printed
- *    *
- *     * Return: Nothing
- *      */
+ * _eputses - prints an input string
+ * @str: the string to be printed
+ *
+ * Return: Nothing
+ */
 void _eputses(char *str)
 {
-		int i = 0;
+	int i = 0;
 
-			if (!str)
-						return;
-				while (str[i] != '\0')
-						{
-									_eputchars(str[i]);
-											i++;
-												}
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchars(str[i]);
+		i++;
+	}
 }
-
-/**
- *  * _eputchars - writes the character c to stderr
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
-int _eputchars(char c)
-{
-		static int i;
-			static char buf[WRITE_BUF_SIZE];
-
-				if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-						{
-									write(2, buf, i);
-											i = 0;
-												}
-					if (c != BUF_FLUSH)
-								buf[i++] = c;
-						return (1);
-}
-
-/**
- *  * _putfds - writes the character c to given fd
- *   * @c: The character to print
- *    * @fd: The filedescriptor to write to
- *     *
- *      * Return: On success 1.
- *       * On error, -1 is returned, and errno is set appropriately.
- *        */
-int _putfds(char c, int fd)
-{
-		static int i;
-			static char buf[WRITE_BUF_SIZE];
-
-				if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-						{
-									write(fd, buf, i);
-											i = 0;
-												}
-					if (c != BUF_FLUSH)
-								buf[i++] = c;
-						return (1);
-}
-
-/**
- *  * _putsfds - prints an input string
- *   * @str: the string to be printed
- *    * @fd: the filedescriptor to write to
- *     *
- *      * Return: the number of chars put
- *       */
-int _putsfds(char *str, int fd)
-{
-		int i = 0;
-
-			if (!str)
-						return (0);
-				while (*str)
-						{
-									i += _putfds(*str++, fd);
-										}
-					return (i);
-}
-
